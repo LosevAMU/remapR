e246a771e630cf0d20ecc2a9ea64d9ca767b84c1
alexeysolo <alexey.solovyev@etu.univ-amu.fr>
2018-03-06 14:44:48 +0100

    Add existing file

---
+++ R/remapR.R
@@ -0,0 +1,9 @@
+#' remapR: package to deal with good and bad bees
+#'
+#' Extremely important package for every Bear
+#'
+#' I've written this package a long time ago in a far-far galaxy.
+#'
+#' @name remapR
+#' @docType package
+NULL
