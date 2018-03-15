f3789e325056b226598dbf1ad6abb897d3b73f69
alexeysolo <alexey.solovyev@etu.univ-amu.fr>
2018-03-15 09:26:27 +0100

    Add existing file

---
+++ R/make_summary_series.R
@@ -0,0 +1,78 @@
+#' @title Load chromosomes sizes file
+#' @author Alexey Solovyev
+#' @description Load preloaded chromosome sizes files.
+#'
+#' @param genome The name of the species to import the chromosomes from.
+#'
+#' @return The path to a file that contains the chromosome lengths.
+#'
+#' @usage loadChromFile <- function(genome)
+#'
+#' @examples
+#' hg19ChromFile <- loadChromFile("hg19")
+#'
+#' @export
+makeSummarySeries <- function(dirFrom="") {
+  if (dirFrom == "") {
+    dirFrom <- path.expand("~/tmp/RData/Output")
+  }
+  dirTo <- dirFrom
+
+  arrayChrom <- gsub(pattern = ".RData", replacement = "", x = grep("^chr", list.files(dirFrom), value = TRUE))
+  # arrayChromTMP <- c("chr20", "chrY")
+  # genomeLen <- length(arrayChrom)
+
+  # arrayChrom <- arrayChromTMP
+
+  # minLim <- rep.int(x = 0, times = genomeLen)
+  # maxLim <- rep.int(x = 0, times = genomeLen)
+  # count <- rep.int(x = 0, times = genomeLen)
+  #
+  # annexe <- data.frame(Chromosome=arrayChrom, minLim, maxLim)
+  # row.names(annexe) <- arrayChrom
+
+  summarySeries <- data.frame()
+  summaryTmp <- data.frame()
+
+
+  # i <- arrayChrom[1]
+  ind_k <- 1
+
+  for (i in arrayChrom) {
+    message(i, ' from ', arrayChrom)
+    nameFileTest <- i
+    fileNameTest <- paste(nameFileTest, ".RData", sep = "")
+    dirTest <- dirFrom
+    myFileTest <- file.path(dirTest, fileNameTest)
+    frameTmp <- get(load(myFileTest))
+    rm(list = c("chr"))
+    listSeries <- unique(frameTmp$ID)
+    # j <- listSeries[1]
+    totSer <- length(listSeries)
+    summaryTmp <- data.frame()
+    for (j in listSeries) {
+      # message(ind_k, ' from ', totSer)
+      frameTmpCalcule <- frameTmp[frameTmp$ID == j, ]
+      summaryTmp[ind_k, "Serie"] <- j
+      summaryTmp[ind_k, "Chromosome"] <- i
+      summaryTmp[ind_k, "Sum_Peaks"] <- nrow(frameTmpCalcule)
+      summaryTmp[ind_k, "TF_Unique"] <- length(unique(frameTmpCalcule$TF))
+      summaryTmp[ind_k, "Cell_Unique"] <- length(unique(frameTmpCalcule$Cell_Mod))
+      ind_k <- ind_k + 1
+    }
+    summarySeries <- rbind(summarySeries, summaryTmp)
+
+
+
+
+  }
+
+  fileNameOutput <- paste("summarySeries", ".RData", sep = "")
+  myFileOutput <- file.path(dirTest, fileNameOutput)
+  save(summarySeries, file=myFileOutput)
+
+
+
+  camelCaps <- "Done"
+  return(camelCaps)
+}
